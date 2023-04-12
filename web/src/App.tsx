ffda37e684a89eab558edb026abd7c72765b6e06
Vitor Markis <vitormarkis2369@gmail.com>
2023-04-12 09:10:14 -0300

    fix message input on chat scrollable div

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -18,62 +18,66 @@ const App: React.FC = () => {
   const submitHandler: SubmitHandler<IMessage> = async formData => {}
 
   return (
-    <div className="h-screen bg-zinc-200 flex flex-col">
-      <div className="shadow-lg bg-white">
+    <div className="h-screen bg-zinc-200 flex flex-col ">
+      <div className="shadow-lg bg-white relative z-10">
         <div className="max-w-7xl w-full px-4 py-3 flex items-center justify-between mx-auto">
-          <div><p className="font-bold my-auto">Logo</p></div>
+          <div>
+            <p className="font-bold my-auto">Logo</p>
+          </div>
           <div></div>
         </div>
       </div>
       <div className="flex items-center justify-center">
         <div className="flex flex-col max-w-4xl w-full border-zinc-300 border-x shadow-lg h-full">
-          <div className="h-[calc(100vh_-_48px_-_90px)] grow bg-indigo-300 flex flex-col gap-3 overflow-x-hidden overflow-y-scroll scroll-thin">
-            <p className="h-12 py-6 bg-red-500">MSG</p>
-            <p className="h-12 py-6 bg-red-500">MSG</p>
-            <p className="h-12 py-6 bg-red-500">MSG</p>
-            <p className="h-12 py-6 bg-red-500">MSG</p>
-            <p className="h-12 py-6 bg-red-500">MSG</p>
-            <p className="h-12 py-6 bg-red-500">MSG</p>
-            <p className="h-12 py-6 bg-red-500">MSG</p>
-            <p className="h-12 py-6 bg-red-500">MSG</p>
-            <p className="h-12 py-6 bg-red-500">MSG</p>
-            <p className="h-12 py-6 bg-red-500">MSG</p>
-            <p className="h-12 py-6 bg-red-500">MSG</p>
-            <p className="h-12 py-6 bg-red-500">MSG</p>
-            <p className="h-12 py-6 bg-red-500">MSG</p>
-            <p className="h-12 py-6 bg-red-500">MSG</p>
-            <p className="h-12 py-6 bg-red-500">MSG</p>
-            <p className="h-12 py-6 bg-red-500">MSG</p>
-            <p className="h-12 py-6 bg-red-500">MSG</p>
-            <p className="h-12 py-6 bg-red-500">MSG</p>
-            <p className="h-12 py-6 bg-red-500">MSG</p>
-            <p className="h-12 py-6 bg-red-500">MSG</p>
+          <div className="h-[calc(100vh_-_48px)] relative grow bg-white overflow-hidden">
+            <div className="overflow-x-hidden overflow-y-scroll flex flex-col gap-3 h-full scroll-thin">
+              <div className="px-5 ">
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, eveniet facilis atque accusamus veritatis sit hic ullam nobis repudiandae non ipsam harum, cum officiis molestiae quae pariatur laudantium accusantium sint?</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, eveniet facilis atque accusamus veritatis sit hic ullam nobis repudiandae non ipsam harum, cum officiis molestiae quae pariatur laudantium accusantium sint?</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, eveniet facilis atque accusamus veritatis sit hic ullam nobis repudiandae non ipsam harum, cum officiis molestiae quae pariatur laudantium accusantium sint?</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, eveniet facilis atque accusamus veritatis sit hic ullam nobis repudiandae non ipsam harum, cum officiis molestiae quae pariatur laudantium accusantium sint?</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, eveniet facilis atque accusamus veritatis sit hic ullam nobis repudiandae non ipsam harum, cum officiis molestiae quae pariatur laudantium accusantium sint?</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, eveniet facilis atque accusamus veritatis sit hic ullam nobis repudiandae non ipsam harum, cum officiis molestiae quae pariatur laudantium accusantium sint?</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, eveniet facilis atque accusamus veritatis sit hic ullam nobis repudiandae non ipsam harum, cum officiis molestiae quae pariatur laudantium accusantium sint?</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, eveniet facilis atque accusamus veritatis sit hic ullam nobis repudiandae non ipsam harum, cum officiis molestiae quae pariatur laudantium accusantium sint?</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, eveniet facilis atque accusamus veritatis sit hic ullam nobis repudiandae non ipsam harum, cum officiis molestiae quae pariatur laudantium accusantium sint?</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, eveniet facilis atque accusamus veritatis sit hic ullam nobis repudiandae non ipsam harum, cum officiis molestiae quae pariatur laudantium accusantium sint?</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, eveniet facilis atque accusamus veritatis sit hic ullam nobis repudiandae non ipsam harum, cum officiis molestiae quae pariatur laudantium accusantium sint?</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, eveniet facilis atque accusamus veritatis sit hic ullam nobis repudiandae non ipsam harum, cum officiis molestiae quae pariatur laudantium accusantium sint?</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, eveniet facilis atque accusamus veritatis sit hic ullam nobis repudiandae non ipsam harum, cum officiis molestiae quae pariatur laudantium accusantium sint?</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, eveniet facilis atque accusamus veritatis sit hic ullam nobis repudiandae non ipsam harum, cum officiis molestiae quae pariatur laudantium accusantium sint?</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, eveniet facilis atque accusamus veritatis sit hic ullam nobis repudiandae non ipsam harum, cum officiis molestiae quae pariatur laudantium accusantium sint?</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, eveniet facilis atque accusamus veritatis sit hic ullam nobis repudiandae non ipsam harum, cum officiis molestiae quae pariatur laudantium accusantium sint?</div>
+                <div>Lorem ips
+              </div>um dolor sit amet consectetur adipisicing elit. Amet, eveniet facilis atque accusamus veritatis sit hic ullam nobis repudiandae non ipsam harum, cum officiis molestiae quae pariatur laudantium accusantium sint?</div>
+              <div className="basis-[90px] shrink-0" />
+            </div>
+            <form
+              onSubmit={handleSubmit(submitHandler)}
+              className="p-6 flex flex-col absolute w-full bottom-0 bg-gradient-to-b from-transparent to-black/50"
+            >
+              <input
+                type="text"
+                hidden
+                {...register("username")}
+                defaultValue={username}
+              />
+              <div className="flex gap-4">
+                <input
+                  type="text"
+                  {...register("message")}
+                  className="bg-white shadow-xl shadow-black/30 outline-none px-6 py-2 rounded-lg border border-zinc-400 text-stone-800 w-full"
+                  placeholder="Sua mensagem..."
+                />
+                <button
+                  type="submit"
+                  className="bg-green-600 rounded-lg w-fit ml-auto font-thin text-white px-6 py-2 shadow-lg shadow-black/30"
+                >
+                  Enviar
+                </button>
+              </div>
+            </form>
           </div>
-        <form
-          onSubmit={handleSubmit(submitHandler)}
-          className="rounded-lg p-6 flex flex-col"
-        >
-          <input
-            type="text"
-            hidden
-            {...register("username")}
-            defaultValue={username}
-          />
-          <div className="flex gap-4">
-          <input
-            type="text"
-            {...register("message")}
-            className="bg-transparent outline-none px-6 py-2 rounded-lg border border-zinc-400 text-stone-800 w-full"
-            placeholder="Sua mensagem..."
-          />
-          <button
-            type="submit"
-            className="bg-green-600 rounded-lg w-fit ml-auto font-thin text-white px-6 py-2 shadow-lg shadow-black/30"
-          >
-            Enviar
-          </button>
-          </div>
-        </form>
         </div>
       </div>
     </div>
